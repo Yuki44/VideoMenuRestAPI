03ad93fdf379eb483ec32395efe72555783d5287
Yuki44 <carl7755@easv365.dk>
2017-10-02 09:29:57 +0200

    Now we talking

---
+++ VideoMenuDAL/IGenreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using VideoMenuDAL.Entities;
+
+namespace VideoMenuDAL
+{
+    public interface IGenreRepository
+    {
+        //C
+        Genre Create(Genre genre);
+        //R
+        List<Genre> GetAll();
+        Genre Get(int Id);
+        //U
+        /// No Update here!
+        //D
+        Genre Delete(int Id);
+    }
+}
